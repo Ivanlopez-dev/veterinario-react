dc570546f2af4e529371495d15787bd59cc91e36
Ivan Lopez <ivaan100@gmail.com>
2022-06-14 19:05:31 +0200

    Añado los tres primeros componentes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,29 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Lucifer</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Iván</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">MorningStar@hell.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">10 Abril de 2022</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+                <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis pariatur, earum dignissimos doloremque laborum rem aut laudantium corrupti, eius ad incidunt rerum? Inventore, maiores error vero iste dignissimos excepturi odio.
+                </span>
+            </p>
+
+    </div>
+  )
+}
+
+export default Paciente
