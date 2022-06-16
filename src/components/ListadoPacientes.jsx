5f7b0506d69d296df6b2c29c78c8af3ad896ad4f
Ivan Lopez <ivaan100@gmail.com>
2022-06-16 13:50:27 +0200

    Añadido texto incondicional al listado de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,24 +1,36 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes}) => {
+const ListadoPacientes = ({ pacientes }) => {
 
   return (
     <div className="md:w-1/2 lg:w-3/5">
 
-        <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
-        <p className="text-xl mt-5 mb-10 text-center">
+      {pacientes && pacientes.length ? (
+
+        <>
+
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
             Administra tus {''}
             <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-        </p>
+          </p>
 
-        { pacientes.map( paciente => (
+          {pacientes.map(paciente => (
             <Paciente
-                key={paciente.id}
-                paciente={paciente}
+              key={paciente.id}
+              paciente={paciente}
             />
-        ))}
-
-        
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   )
 }
