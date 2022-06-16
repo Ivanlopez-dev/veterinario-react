6c2e158fee99d14fa7fcac52722932d764c9449a
Ivan Lopez <ivaan100@gmail.com>
2022-06-16 13:23:40 +0200

    Creado key unico(id) para cada paciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,29 +1,31 @@
-const Paciente = () => {
-  return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+const Paciente = ({ paciente }) => {
+
+    const { nombre, propietario, email, fecha, sintomas } = paciente
+
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
-                <span className="font-normal normal-case">Lucifer</span>
+                <span className="font-normal normal-case">{nombre}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
-                <span className="font-normal normal-case">Iván</span>
+                <span className="font-normal normal-case">{propietario}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
-                <span className="font-normal normal-case">MorningStar@hell.com</span>
+                <span className="font-normal normal-case">{email}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
-                <span className="font-normal normal-case">10 Abril de 2022</span>
+                <span className="font-normal normal-case">{fecha}</span>
             </p>
 
             <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
-                <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis pariatur, earum dignissimos doloremque laborum rem aut laudantium corrupti, eius ad incidunt rerum? Inventore, maiores error vero iste dignissimos excepturi odio.
-                </span>
+                <span className="font-normal normal-case">{sintomas}</span>
             </p>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Paciente
