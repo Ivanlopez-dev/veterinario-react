55204f255b45e52f0502657f5e4d35e17631239c
Ivan Lopez <ivaan100@gmail.com>
2022-06-21 13:13:17 +0200

    Detectando nuevo registro o edición

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Error from './Error';
 
-const Formulario = ({ pacientes, setPacientes, paciente }) => {
+const Formulario = ({ pacientes, setPacientes, paciente, setPaciente }) => {
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
     const [email, setEmail] = useState('');
@@ -45,11 +45,24 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
             propietario, 
             email, 
             fecha, 
-            sintomas,
-            id: generarId()
+            sintomas
         }
 
-        setPacientes([...pacientes, objetoPaciente]);
+        if(paciente.id) {
+            // Editando el Registro
+            objetoPaciente.id = paciente.id
+
+            const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id ===
+            paciente.id ? objetoPaciente : pacienteState )
+
+            setPacientes(pacientesActualizados)
+            setPaciente({})
+
+        } else {
+            // Nuevo Registro
+            objetoPaciente.id = generarId();
+            setPacientes([...pacientes, objetoPaciente]);
+        }
 
         //Reiniciar el form
         setNombre('')
